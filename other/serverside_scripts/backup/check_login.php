2cdd11bf80ab9d85ea956716d38de0079375e5d6
Shehzad Azram <shez1983@hotmail.co.uk>
2013-03-17 18:13:47 +0000

    cleaning dierectory

--- other/serverside_scripts/backup/check_login.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if ( isset( $_SESSION['user_id'] ) ){
-		echo TRUE;
-	} else { 
-		echo FALSE;
-	}
-?>
--- other/serverside_scripts/backup/dbconn.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$db = new PDO('mysql:host=mysql.shehzadazram.co.uk;dbname=shez_education;charset=utf8', 'shez_student', '!Qawpli£34'
-);
-?>
